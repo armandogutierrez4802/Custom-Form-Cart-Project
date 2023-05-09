dbb8a5986516db5f49042adaaba14f1d683b2e7c
armandogutierrez4802 <armandogutierrez4802@gmail.com>
2023-05-09 16:33:20 -0700

    working on second useEffect

--- src/App.js
+++ src/App.js
@@ -14,8 +14,14 @@ export default function App() {
   // const fillingOptions = [];
   // const options = []; // Like adding nuts, fruit, etc.... maybe put fillings here too?
 
-  // const [count, setCount] = useState(0);
-  const [count, setCount] = useState(parseInt(localStorage.getItem('counter')));
+  const [count, setCount] = useState(0);
+  // const initialValue = localStorage.getItem('counter') ? parseInt(localStorage.getItem('counter')) : 0;
+  // const [count, setCount] = useState(initialValue);
+  // const [count, setCount] = useState(parseInt(localStorage.getItem('counter')));
+
+ useEffect(() => {
+
+ }, []);
 
   useEffect(() => {
     localStorage.setItem('counter', count);
