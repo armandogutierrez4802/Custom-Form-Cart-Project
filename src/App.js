5631529878981613d52c159ca57f9304ffbf15a6
armandogutierrez4802 <armandogutierrez4802@gmail.com>
2023-05-09 16:20:26 -0700

    local storage test

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect} from 'react';
 import Feature from './Feature';
 import {
   initialCartItems,
@@ -14,8 +14,20 @@ export default function App() {
   // const fillingOptions = [];
   // const options = []; // Like adding nuts, fruit, etc.... maybe put fillings here too?
 
+  const [count, setCount] = useState(localStorage.getItem('count'));
+
+  useEffect(() => {
+    localStorage.setItem('count', count+1);
+  }, [count])
+
+  const handleClick = () => {
+    // setCount(count+1);
+  }
+
   return (
     <>
+    <h1>{count}</h1>
+    <button type="button" onClick={handleClick}>+</button>
       <Feature
         initialCartItems={initialCartItems}
         categories={categories}
